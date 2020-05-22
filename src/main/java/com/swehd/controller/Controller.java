290cf10f2ada7391a4488dd1f891af0ef199bf66
David Halasz <davidhalasz22@gmail.com>
2020-05-22 22:00:46 +0200

    Save images to resources/pictures

--- src/main/java/com/swehd/controller/Controller.java
+++ src/main/java/com/swehd/controller/Controller.java
@@ -7,14 +7,22 @@ import com.swehd.user.UserDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.swing.text.html.ImageView;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 public class Controller{
     public long id;
@@ -31,36 +39,27 @@ public class Controller{
     }
 
 
+    @FXML
+    private AnchorPane postWall;
+    @FXML
+    private ImageView pimage;
+    @FXML
+    private Label pname;
+    @FXML
+    private Label pdescription;
 
 
-    /*
-        private List<PhotoSharing> photoSharings;
 
-        @FXML
-        private ListView<PhotoSharing> postWall;
+    public void initialize() {
+        postDao = PostDao.getInstance();
 
+        List<Post> postWall = postDao.findAll();
+        pdescription.setLabelFor(new PropertyValueFactory<>("description"));
+        
 
-        public void initialize() {
-            photoSharings = new ArrayList<PhotoSharing>();
-            postWall.getItems().setAll(photoSharings);
-            postWall.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        }
-
-        @FXML
-        private TextArea photoDesc;
-
-        @FXML
-        private Label deadlineLabel;
+    }
 
 
-        @FXML
-        public void handleClickPost() {
-            PhotoSharing post = postWall.getSelectionModel().getSelectedItem();
-            photoDesc.setText(post.getDescription());
-            deadlineLabel.setText(post.getDeadLine().toString());
-        }
-
-    */
 
     @FXML
     private Button choosePicBtn;
@@ -70,10 +69,14 @@ public class Controller{
     private TextArea postDesc;
 
     private String filename;
+    private String path;
 
     public void initFile(String selectedFile) {
         this.filename = selectedFile;
+    }
 
+    public void absolutePath(String path) {
+        this.path = path;
     }
 
     /**
@@ -105,7 +108,7 @@ public class Controller{
      */
     @FXML
     private void postForm(ActionEvent e) throws IOException {
-        if (e.getSource().equals(choosePicBtn)){
+        if (e.getSource().equals(choosePicBtn)) {
             FileChooser fileChooser = new FileChooser();
             fileChooser.getExtensionFilters().addAll(
                     new FileChooser.ExtensionFilter("JPG", "*.jpg"),
@@ -116,18 +119,37 @@ public class Controller{
 
             if (selectedFile != null) {
                 initFile(selectedFile.getName());
+                absolutePath(selectedFile.getAbsolutePath());
             }
 
         } else if (e.getSource().equals(sendPostBtn)) {
-            if(postDesc.getText() != null && filename != null) {
+            if (postDesc.getText() != null && filename != null) {
                 postDao = PostDao.getInstance();
                 postDao.persist(createPost());
+
+                File folder = new File("src/main/resources/pictures");
+                boolean success = folder.mkdir();
+                String destination = folder.getAbsolutePath() + File.separator + filename;
+
+                try {
+                    // Copy file from source to destination
+                    FileChannel source = new FileInputStream(path).getChannel();
+                    FileChannel dest = new FileOutputStream(destination).getChannel();
+                    dest.transferFrom(source, 0, source.size());
+
+                    // Close
+                    source.close();
+                    dest.close();
+
+                    System.out.println("Done");
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
             } else {
                 System.out.println("Empty");
             }
         }
-
     }
-
-
 }
